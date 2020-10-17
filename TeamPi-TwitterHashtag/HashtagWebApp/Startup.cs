a5c48609f37a7d6e6d83dcd7f88fca4339f2cfbf
Tammi Jo <71151830+tanthony2020@users.noreply.github.com>
2020-10-17 18:55:16 -0400

    added datatables for front end

--- TeamPi-TwitterHashtag/HashtagWebApp/Startup.cs
+++ TeamPi-TwitterHashtag/HashtagWebApp/Startup.cs
@@ -48,11 +48,11 @@ namespace HashtagWebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+               // app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -67,6 +67,9 @@ namespace HashtagWebApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
--- TeamPi-TwitterHashtag/HashtagWebApp/wwwroot/js/site.js
+++ TeamPi-TwitterHashtag/HashtagWebApp/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function () {
+    var tweettable = $('#tweets').DataTable({
+        "bAutoWidth": true,
+        scrollY: '50vh',
+        responsive: true,
+        scrollCollapse: true,
+        "order": [[1, "DESC"]]
+    });
+   // tweettable.height = "100px";
+});
